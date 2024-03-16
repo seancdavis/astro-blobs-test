73d9bd56da730679020cbd934e7b4d7588f10b5d
Sean C Davis <scdavis41@gmail.com>
2024-03-16 15:53:20 -0400

    Adjust netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
-
-import netlify from "@astrojs/netlify";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
